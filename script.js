c5366f78a992868500bc28ec0cc8479e8d88f961
bacf5 <brunoagustincarusofassa@gmail.com>
2022-09-17 19:09:10 -0300

    Added buttons with notification

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
   Notification.requestPermission().then((perm) => {
-    alert(perm);
+    if (perm === 'granted') new Notification('Ejemplo de botón');
+  });
+});
+
+const button1 = document.querySelector('.button-1');
+
+button1.addEventListener('click', () => {
+  Notification.requestPermission().then((perm) => {
+    if (perm === 'granted')
+      new Notification('Ejemplo de botón 1 con body', {
+        body: 'Acá hay más información',
+      });
+  });
+});
+
+const button2 = document.querySelector('.button-2');
+
+button2.addEventListener('click', () => {
+  Notification.requestPermission().then((perm) => {
+    if (perm === 'granted')
+      new Notification('Ejemplo de botón 2', {
+        body: 'Notificación con logo',
+        icon: 'alert-png.png',
+      });
   });
 });
